138373580c61e6e00d0ba5473f33f047f10ebd8f
punk0706 <punk0706@gmail.com>
2014-12-20 08:23:11 -0500

    try

--- phonegapIframe_phonegapCode.js
+++ phonegapIframe_phonegapCode.js
@@ -92,6 +92,7 @@ window.onNotification = function (e) {
 
 window.sendMessageToPlatform = function (message) {
   alert("sendMessageToPlatform:" + JSON.stringify(message));
+  window.parent.postMessage(message, "*");
   //window.document.getElementById("platform_iframe").contentWindow.postMessage(message, "*");
 }
 window.sendToken = function (token, error) {
